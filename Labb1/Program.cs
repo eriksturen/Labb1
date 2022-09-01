3554339e1104cd8303dba2a3d3a209856022e7a1
eriksturen <eriksturen@gmail.com>
2022-09-01 15:17:32 +0200

    Add project files.

---
+++ Labb1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Labb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // program prompts user to enter a text string to be searched 
+            Console.WriteLine("Enter a string to search through: ");
+
+
+        }
+    }
+}
