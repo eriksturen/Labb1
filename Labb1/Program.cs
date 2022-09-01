617c9f88df419090e56e10ff85dece755ee363da
eriksturen <eriksturen@gmail.com>
2022-09-01 15:38:53 +0200

    Some form of start - string can be entered and searched for numbers?

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -6,8 +6,30 @@
         {
             // program prompts user to enter a text string to be searched 
             Console.WriteLine("Enter a string to search through: ");
+            string input = Console.ReadLine();
+            
 
+            // text string is to be searched for substrings with numbers 
+            // the substrings should start and end with the same number 
+            // but not contain anything other than numbers 
+            // for example input: 242m500005 
+            // the program should tell us that there are two substrings of interest here:
+            // 242 and 5000005
 
+            // the program will tell us this by printing the full input string with 
+            // each substring marked in a different color to the console
+            // this happens once for each substring, so two times on this example. 
+
+            // all substring numbers are also to be added - this needs a long instead of an int
+            // because very large number is possible 
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    Console.Write(input[i]);
+                }
+            }
         }
     }
 }
