e55281fae21ae175f683bda2f33201a007ec6280
eriksturen <eriksturen@gmail.com>
2022-09-05 15:36:24 +0200

    Found a bug with using i+2 on row 88 - it allows letters to reenter the strings.

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -85,7 +85,7 @@
                 {
                     // for the rest of the string (beginning one index to the right)
                     // search for the same number again
-                    for (int j = i+2; j < input.Length; j++)
+                    for (int j = i+1; j < input.Length; j++)
                     {
                         bool isIntAgain = int.TryParse(input[j].ToString(), out int numberAgain);
                         // skip round if it's anything other than a number
@@ -112,6 +112,8 @@
                     }
                 }
             }
+
+            long[] numbersToSum = new long[numberStringsFound];
 
             // now, for the amount of correct strings found, print out the whole string again
             // coloring the ones between what is stored in start/endindexes 
